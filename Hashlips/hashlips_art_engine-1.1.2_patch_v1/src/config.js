abd47197974be35f1bf0b40f04a825806a54302e
G Brezeanu <gbrezeanu@protonmail.com>
2021-12-12 13:59:56 +0200

    tongue

--- Hashlips/hashlips_art_engine-1.1.2_patch_v1/src/config.js
+++ Hashlips/hashlips_art_engine-1.1.2_patch_v1/src/config.js
@@ -26,7 +26,7 @@ const solanaMetadata = {
 };
 
 const layerConfigurations = [{
-    growEditionSizeTo: 1000,
+    growEditionSizeTo: 100,
     layersOrder: [
       {name: "Background"},
       {name: "Breed"},
